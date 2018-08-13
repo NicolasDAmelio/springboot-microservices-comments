abfe8f205997f37bb8822bb688377e8963357d79
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-13 16:39:59 -0300

    first commit

---
+++ src/main/java/com/ndamelio/learning/springbootmicroservicesimages/SpringbootMicroservicesImagesApplication.java
@@ -0,0 +1,12 @@
+package com.ndamelio.learning.springbootmicroservicesimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroservicesImagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroservicesImagesApplication.class, args);
+    }
+}
